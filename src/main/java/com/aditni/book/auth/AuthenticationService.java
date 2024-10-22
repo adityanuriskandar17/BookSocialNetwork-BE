be01bcbbfb18f20128bc603dce4ef853a8dd493b
Aditya Nur Iskandar <aditya.iskandar@alodokter.com>
2024-10-22 11:26:45 +0700

    Before add email service

--- src/main/java/com/aditni/book/auth/AuthenticationService.java
+++ src/main/java/com/aditni/book/auth/AuthenticationService.java
@@ -1,17 +1,26 @@
 package com.aditni.book.auth;
 
 import com.aditni.book.role.RoleRepository;
+import com.aditni.book.user.Token;
+import com.aditni.book.user.TokenRepository;
 import com.aditni.book.user.User;
+import com.aditni.book.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    private RoleRepository roleRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 
     public void register(RegistrationRequest request) {
         var userRole = roleRepository.findByName("USER")
@@ -21,10 +30,40 @@ public class AuthenticationService {
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())
                 .email(request.getEmail())
-                .password()
+                .password(passwordEncoder.encode(request.getPassword()))
                 .accountLocked(false)
                 .enabled(false)
                 .roles(List.of(userRole))
                 .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+        // send email
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        // generate a token
+        String generateToken = generateAndSaveActivationToken(6);
+        var token = Token.builder()
+                .token(generateToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generateToken;
+    }
+    private String generateAndSaveActivationToken(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length()); //0..9
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
     }
 }
